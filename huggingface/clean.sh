f679e91a304653df1ab33b372536a4d89176ec78
Atharv Chowdhary <atharv_chowdhary@brown.edu>
2025-06-08 16:24:12 +0800

    temporary

--- huggingface/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname $0)/hf"
-rm -rf model data outputs core/checkpoints eval/interpret.txt
--- huggingface/core/core.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-BLUE='\033[1;34m'
-RESET='\033[0m'
-
-echo -e "${BLUE}Running core tokenization and training pipeline...${RESET}"
-cat ../data/train.txt | python3 tkn.py | python3 chunk.py | python3 train.py
-
-rm -rf checkpoints

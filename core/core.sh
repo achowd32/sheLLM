3885e4f56594700aaee0d34da505b1db322a45be
Atharv Chowdhary <atharv_chowdhary@brown.edu>
2025-06-13 00:30:02 +0800

    changed sample.sh to tokenize.sh

--- core/core.sh
+++ core/core.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 filename='model.pth'
+num_samples=$((MAX_ITERS * BATCH_SIZE))
 
 echo -e "${BLUE}Initiating core tokenization and training loop...${RESET}"
-./sample.sh $((MAX_ITERS * BATCH_SIZE)) | 
+./tokenize.sh $num_samples | 
 python3 chunk.py $BATCH_SIZE $BLOCK_SIZE |
 python3 train.py $EVAL_INTERVAL $MAX_ITERS $filename |
 ./log.sh 
--- core/log.sh
+++ core/log.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 num_samples=$((EVAL_ITERS * BATCH_SIZE))
 while read log; do
-    tloss=$(./sample.sh $num_samples | 
+    tloss=$(./tokenize.sh $num_samples | 
     python3 chunk.py $BATCH_SIZE $BLOCK_SIZE |
     python3 log.py "../logs/${log}.pth" $EVAL_ITERS |
     grep -oE "[0-9]+\.[0-9]+")
 
-    vloss=$(./sample.sh $num_samples -v |
+    vloss=$(./tokenize.sh $num_samples -v |
     python3 chunk.py $BATCH_SIZE $BLOCK_SIZE |
     python3 log.py "../logs/${log}.pth" $EVAL_ITERS |
     grep -oE "[0-9]+\.[0-9]+")

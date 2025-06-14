ca56d877f2e95008c840bbff05ea8d5141a11eb8
Atharv Chowdhary <atharv_chowdhary@brown.edu>
2025-06-14 19:15:45 +0800

    added iterations for evaluations

--- eval/eval.sh
+++ eval/eval.sh
@@ -2,28 +2,51 @@
 prompt=""
 model_file="model.pth"
 max_tokens=500
+num_evals=1
 
-# generate text sample and save it; will be used for evaluations
-echo -e "${BLUE}Generating text with trained model...${RESET}"
-echo -ne "$prompt" | ./encode.sh | python3 generate.py $model_file $max_tokens | ./decode.sh > sample.txt
-echo "Text saved to eval/sample.txt"
+lang_sum=0
+prec_sum=0
+rec_sum=0
+f1_sum=0
+pos_sum=0
 
-# load the sample text and reference text into variables
-sample=$(cat sample.txt)
-reference=$(head -c +"$max_tokens" ../data/val.txt)
+echo -e "${BLUE}Performing evaluations...${RESET}"
 
-# perform a simple language evaluation: number of language errors in sample
-echo -e "${BLUE}Performing language evaluation...${RESET}"
-numerrors=$(python3 lang_eval.py "$sample" 2>&1 | grep "Numerrors: " | awk '{print $2}')
-echo "This text has ${numerrors} errors"
+iters=0
+while [ $iters -lt $num_evals ]; do
+    # generate text sample and save it; will be used for evaluations
+    echo -ne "$prompt" | ./encode.sh | python3 generate.py $model_file $max_tokens | ./decode.sh > sample.txt
 
-# perform an evaluation of the semantic similarity between sample and validation data
-echo -e "${BLUE}Performing BERT evaluation...${RESET}"
-bertscores=$(python3 bert_eval.py "$sample" "$reference" 2>&1 | grep "BERTScores:" | awk '{print $2, $3, $4}')
-read precision recall f1 <<< "$bertscores"
-echo -e "Precision Score: $precision\nRecall Score: $recall\nF1 Score: $f1"
+    # load the sample text and reference text into variables
+    sample=$(cat sample.txt)
+    reference=$(head -c +"$max_tokens" ../data/val.txt)
 
-# perform an evaluation of the syntactic similarity between sample and validation data
-echo -e "${BLUE}Performing Part-Of-Speech evaluation...${RESET}"
-posscore=$(python3 pos_eval.py "$sample" "$reference" 2>&1 | grep "Syntactic similarity" | awk '{print $3}')
-echo "Syntactic similarity: ${posscore}"
+    # perform a simple language evaluation: number of language errors in sample
+    lang_score=$(python3 lang_eval.py "$sample" 2>&1 | grep "Numerrors: " | awk '{print $2}')
+    ((lang_sum += lang_score))
+
+    # perform an evaluation of the semantic similarity between sample and validation data
+    bertscores=$(python3 bert_eval.py "$sample" "$reference" 2>&1 | grep "BERTScores:" | awk '{print $2, $3, $4}')
+    read precision recall f1 <<< "$bertscores"
+    prec_sum=$(bc -le "$prec_sum + $precision")
+    rec_sum=$(bc -le "$rec_sum + $recall")
+    f1_sum=$(bc -le "$f1_sum + $f1")
+
+    # perform an evaluation of the syntactic similarity between sample and validation data
+    pos_score=$(python3 pos_eval.py "$sample" "$reference" 2>&1 | grep "Syntactic similarity" | awk '{print $3}')
+    pos_sum=$(bc -le "$pos_sum + $pos_score")
+
+    # iterate
+    ((iters+=1))
+done 
+
+lang_avg=$(bc -e "scale=5; $lang_sum / $num_evals")
+prec_avg=$(bc -e "scale=5; $prec_sum / $num_evals")
+rec_avg=$(bc -e "scale=5; $rec_sum / $num_evals")
+f1_avg=$(bc -e "scale=5; $f1_sum / $num_evals")
+pos_avg=$(bc -e "scale=5; $pos_sum / $num_evals")
+
+echo -e "${BLUE}Printing evaluation results, averaged over ${num_evals} iterations...${RESET}"
+echo "Language evaluation: ${lang_avg} language errors detected on average"
+echo -e "Semantic similarity evaluation (BERT):\nPrecision—$precision\nRecall Score—$recall\nF1 Score—$f1"
+echo "Syntactic similarity evaluation (Part-Of-Speech): ${pos_avg}"
